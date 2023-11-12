e08ab32ae1f8621be692a454bd0387dc7f376e1d
nestornumber <nestor.rodriguez999@gmail.com>
2023-11-12 13:05:37 -0600

    nav bar funcionando

--- app/src/main/java/sv/edu/itca/foodfast/HomeActivity.java
+++ app/src/main/java/sv/edu/itca/foodfast/HomeActivity.java
@@ -1,8 +1,13 @@
 package sv.edu.itca.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home){
+                    return true;
+                }
+                if (id == R.id.search){
+                    startActivity(new Intent(getApplicationContext()
+                            ,SearchActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                if (id == R.id.add){
+                    startActivity(new Intent(getApplicationContext()
+                            ,AddActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                if (id == R.id.account){
+                    startActivity(new Intent(getApplicationContext()
+                            ,AccountActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
