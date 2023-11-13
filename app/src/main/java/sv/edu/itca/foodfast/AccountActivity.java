b05818c959bd0c2548911054fd442a18a934efb4
L3onli <leonel.martinez22@itca.edu.sv>
2023-11-12 22:53:35 -0600

    Cuenta y Login

--- app/src/main/java/sv/edu/itca/foodfast/AccountActivity.java
+++ app/src/main/java/sv/edu/itca/foodfast/AccountActivity.java
@@ -1,11 +1,15 @@
 package sv.edu.itca.foodfast;
 
+import static sv.edu.itca.foodfast.R.id.textViewUsuario;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,7 +20,24 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        // Obtén la información del usuario del Intent
+        String usuario = getIntent().getStringExtra("USUARIO");
+        String contraseña = getIntent().getStringExtra("CONTRASEÑA");
+
+        // Muestra la información del usuario en TextViews
+        TextView textViewUsuario = findViewById(R.id.textViewUsuario);
+        TextView textViewContraseña = findViewById(R.id.textViewContraseña);
+
+        textViewUsuario.setText("Usuario: " + usuario);
+        textViewContraseña.setText("Contraseña: " + contraseña);
+
+
+        // Configuración de la barra de navegación inferior
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.account);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setSelectedItemId(R.id.home);
 
@@ -49,4 +70,6 @@ public class AccountActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/sv/edu/itca/foodfast/IniciarSesionActivity.java
+++ app/src/main/java/sv/edu/itca/foodfast/IniciarSesionActivity.java
@@ -2,13 +2,46 @@ package sv.edu.itca.foodfast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+    private EditText editTextUsuario;
+    private EditText editTextContraseña;
+    private Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        editTextUsuario = findViewById(R.id.editText);
+        editTextContraseña = findViewById(R.id.editText1);
+        btnIniciarSesion = findViewById(R.id.button2);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Obtén la información del usuario
+                String usuario = editTextUsuario.getText().toString();
+                String contraseña = editTextContraseña.getText().toString();
+
+                // Crea un Intent para pasar a la siguiente actividad (AccountActivity)
+                Intent intent = new Intent(IniciarSesionActivity.this, AccountActivity.class);
+
+                // Pasa la información del usuario al Intent
+                intent.putExtra("USUARIO", usuario);
+                intent.putExtra("CONTRASEÑA", contraseña);
+
+                // Inicia la siguiente actividad
+                startActivity(intent);
+            }
+        });
+
     }
 }
