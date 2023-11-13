07597f4e5b393f33ad14ba01396e591da33af6dd
nestornumber <nestor.rodriguez999@gmail.com>
2023-11-12 19:09:54 -0600

    registrarse

---
+++ app/src/main/java/sv/edu/itca/foodfast/RegistrarseActivity1.java
@@ -0,0 +1,14 @@
+package sv.edu.itca.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarseActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse1);
+    }
+}
