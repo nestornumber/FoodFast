669e7ba7cf12e32e9ede3d5d5d2b23e891383410
nestornumber <nestor.rodriguez999@gmail.com>
2023-11-14 23:39:49 -0600

    arreglar añadir

---
+++ app/src/main/java/sv/edu/itca/foodfast/AcercaDeLaAppActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.itca.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcercaDeLaAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de_la_app);
+    }
+}
